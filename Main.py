53ad2823a0013f0035162362b171e28e2dc0e8c4
CrystalWang2002 <wangbi200201@gmail.com>
2024-07-02 20:14:27 +0800

    first commit

---
+++ Main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Main Page")
+
+st.write("""
+### 🚀 Hello, Welcome to Amazon Sales Data Analysis project! 
+
+### 🔍Dataset Introduction:
+In this project, I will analyze Amazon Sales Dataset. This dataset contains 1k+ pieces of specific information 
+         about Amazon products with product ids, prices, discounts, categories, customer reviews and other important 
+         details. Analyzing this Dataset will provide in-depth insights into market trends, consumer behavior, 
+         pricing strategies, and product performance to support more effective business decisions.
+
+### 📊Exploratory Data Analysis:
+I will conduct an exploratory data analysis mainly focusing on the following aspects:
+- Sales Distribution
+- Discount Distribution
+- Customer Rating and Reviews
+- Correlation Analysis
+         
+### 📈Recommendation System:
+Finally, I will build a content-based recommendation system that recommends similar products to the user based on their purchasing history.
+You can type the user id and get product recommendations for that user.
+
+✨Let's GO!🌐✨
+""")
---
+++ pages/2 Recommendation System.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import function as func
+import numpy as np
+# part 8
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Title of the app
+st.title('Recommendation System for Amazon Products')
+st.write("Fill in the following information to get the recommendation 👩🏻‍💻")
+#insert pictures
+col1, col2 = st.columns(2)
+with col1:
+    st.image("./pictures/Amazon.jpg", use_column_width=True)
+with col2:
+    st.image("./pictures/Amazon1.png", use_column_width=True)
+
+df = func.load_data()
+list=np.arange(0,1192,1)
+user_id_encoded = st.selectbox("Select the User ID you want to give recommendation:", list)
+top_n = st.number_input("Type in the number of recommend items you want:", min_value=1, max_value=20, step=1, format="%d")
+
+if st.button("Recommend"):
+    st.write(func.recommend_products(df, user_id_encoded, top_n))
+else:
+    st.write(func.recommend_products(df, 0, 5))
